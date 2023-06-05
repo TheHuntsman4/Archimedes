f4e39eece0f40f1dbcb6dafd0e16325f38905571
TheHuntsman4 <anikethvij464@gmail.com>
2023-06-06 02:17:16 +0530

    feat- changes the colour theme

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { useState } from 'react';
 import logo from '../images/archimedes_logo.svg'
 import {FiMenu} from 'react-icons/fi';
+import './styles/navbar.css';
 
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3  mb-3 h-[10%] bg-[#DCCFC0]">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3  mb-3 h-[10%] bg-[#DCCFC0] rounded-full mt-[2rem]">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between bg-[#DCCFC0]">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start bg-[#DCCFC0]">
-          <img src={logo} className=' h-[10vh] w-[10vh] bg-[#DCCFC0]' />
+          <img src={logo} className='p-[2rem] bg-[#DCCFC0] hide-in-mobile' height={"200px"} width={"200px"} />
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none bg-[#DCCFC0]"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-            <FiMenu className='text-[#deb992]' />
+            <FiMenu className='text-[#080A22]' />
             </button>
           </div>
           <div
